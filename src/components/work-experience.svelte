e6d94eb9fed4e8a282caf5f0028c1e3b2d1ea650
guhkun13 <guhkun13@gmail.com>
2022-05-31 11:50:12 +0800

    add work exp as it manager pt makara mas

--- src/components/work-experience.svelte
+++ src/components/work-experience.svelte
@@ -8,6 +8,8 @@
     </div>
     
     <div class="w-full px-4 flex flex-wrap justify-center">
+      
+      <!-- ITF  -->
       <div class="mb-12 p-4 bg-slate-200">        
         <p class="text-xl font-semibold font-mono mt-2 mb-2"> Programmer
           <span class="text-purple-500"> | </span> 
@@ -24,21 +26,20 @@
           <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> Networking </span>
           <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> Raspberryi Pi </span>
           <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> Smartcard </span>
-          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> Card Reader </span>
-          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> ZeroMQ </span>          
+          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> Card Reader </span>          
         </div>
         
         <p class="font-base mt-5"> Some of my works: </p>        
         
-        <div class="bg-indigo-200 p-5 m-5 rounded-md border-dashed border-2 border-indigo-500">
+        <div class="bg-blue-200 p-5 m-5 rounded-md border-dashed border-2 border-indigo-500">
           <h2 class="uppercase font-bold font-2xl"> 1. siparkir </h2>                    
           <p> Sistem Informasi Parkir (Parking Information System). </p>
           <hr class=" border-black mb-4">
           <p class="font-thin"> <b> As fullstack web developer</b>, using Django and DRF I was responsible to create web app to record, monitor and manage users on parking system. It is also to generate statistic and timely report </p>
-          <p class="font-thin mt-2"> <b> As IoT Developer, </b> I was responsible to research and test the parking hardware such as barrier gate, loop induction and connect it to custom made controller built using Raspberry Pi by implementing Event Driven Arch using ZeroMQ, then integrate it with web app as the auth and logging server. The identifier was using UI smartcard identifier (staff or student card) and also using e-KTP for some person </p>
+          <p class="font-thin mt-2"> <b> As IoT Developer, </b> I was responsible to research and test the parking hardware such as barrier gate, loop induction and connect it to custom made controller built using Raspberry Pi then integrate it with web app as the auth and logging server. The identifier was using UI smartcard identifier (staff or student card) and also using e-KTP for some person </p>
         </div>
         
-        <div class="bg-blue-200 p-5 m-5 rounded-md border-dashed border-2 border-blue-500">
+        <div class="bg-indigo-200 p-5 m-5 rounded-md border-dashed border-2 border-blue-500">
           <h2 class="uppercase font-bold font-2xl"> 2. sipintu </h2>                    
           <p> Sistem Informasi Pintu (Door Information System). </p>
           <hr class=" border-black mb-4">
@@ -57,6 +58,87 @@
           </p>          
         </div>
       </div>                    
+      
+      <!-- Pusilkom UI -->
+      <div class="mb-12 p-4 bg-slate-200">        
+        <p class="text-xl font-semibold font-mono mt-2 mb-2"> Software Engineer
+          <span class="text-purple-500"> | </span> 
+          <span class="text-sm font-semibold mb-2">  Pusilkom UI  </span>
+          <span class="text-purple-500"> | </span> 
+          <span class="text-sm font-semibold mb-2">  February 2018 - June 2019 </span> 
+        </p>           
+        <p class="font-secondary font-thin "> Responsible for maintaining and developing web application
+        </p>
+        <div class="text-xs mt-2"> Tech: 
+          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> Python </span>                              
+          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> Raspberry Pi </span>
+          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> Smartcard </span>
+          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> Card Reader </span>
+          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> ZeroMQ </span>          
+          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> Bash scripting </span>
+          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> Event Driven Arch </span>
+        </div>        
+        <p class="font-base mt-5"> Some of my works: </p>        
+        
+        <div class="bg-blue-200 p-5 m-5 rounded-md border-dashed border-2 border-indigo-500">
+          <h2 class="uppercase font-bold font-2xl"> 1. Presentronik </h2>                    
+          <p> Online presence system </p>
+          <hr class=" border-black mb-4">
+          <p class="font-thin"> My role was as research assistant and was responsible to develop IoT device using Raspberry Pi as the controller, connected to card-reader to read smartcard UI as the identifier, using the same concept with siparkir. To further increase the response time, we implement Event Driven Architecture using ZeroMQ, by using pub/sub messaging between sensor controller actuator. </p>
+        </div>
+        
+        <div class="bg-indigo-200 p-5 m-5 rounded-md border-dashed border-2 border-indigo-500">
+          <h2 class="uppercase font-bold font-2xl"> 2. Maintenance </h2>                    
+          <p> Maintenance of SIPARKIR and SIPINTU </p>
+          <hr class=" border-black mb-4">
+          <p class="font-thin"> Maintain existing web-application implement research result from Presentronik which has improved response time, had better handling error response and less coupling components to SIPARKIR and SIPINTU </p>
+        </div>
+      </div>   
+      
+      <!-- PT Makara Mas -->
+      <div class="mb-12 p-4 bg-slate-200">        
+        <p class="text-xl font-semibold font-mono mt-2 mb-2"> IT Manager
+          <span class="text-purple-500"> | </span> 
+          <span class="text-sm font-semibold mb-2">  PT Makara Mas </span>
+          <span class="text-purple-500"> | </span> 
+          <span class="text-sm font-semibold mb-2">  August 2019 - May 2020 </span> 
+        </p>           
+        <p class="font-secondary font-thin "> Responsible for planning, managing and executing IT activities within company
+        </p>
+        <div class="text-xs mt-2"> Tech: 
+          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> PHP </span>
+          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> Yii Framework </span>
+          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> Scrum </span>
+          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> Docker </span>
+          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> Server Maintenance </span>
+          <span class="bg-sky-200 text-blue-700 rounded px-1 py-1 mx-1"> ISO 8583 </span>
+        </div>        
+        <p class="font-base mt-5"> Some of my works: </p>        
+        
+        <div class="bg-blue-200 p-5 m-5 rounded-md border-dashed border-2 border-indigo-500">
+          <h2 class="uppercase font-bold font-2xl"> 1. Edupay </h2>                    
+          <p> Develop & Maintenance of Edupay </p>
+          <hr class=" border-black mb-4">
+          <p class="font-thin"> Web Application Edupay is one of the main product of PT Makara Mas. It is web app to store and manage invoices and check payment record. Some roles exists within the app are: BILLER (invoices owner), BANK and SWITCHER (PT Makara Mas as the mediator between clients and bank) </p>
+        </div>
+        
+        <div class="bg-indigo-200 p-5 m-5 rounded-md border-dashed border-2 border-indigo-500">
+          <h2 class="uppercase font-bold font-2xl"> 2. Edupay & Pepipost </h2>                    
+          <p> Integrating Edupay with Pepipost </p>
+          <hr class=" border-black mb-4">
+          <p class="font-thin"> Pepipost (netcore) is third party Email Delivery app that has many functions needed to send email. Especially for Edupay that sending many invoices or payments notification via emails, good and reliable Email Delivery was needed.             
+          </p>
+        </div>
+
+        <div class="bg-violet-200 p-5 m-5 rounded-md border-dashed border-2 border-indigo-500">
+          <h2 class="uppercase font-bold font-2xl"> 3. BTNS Mobile Payment </h2>                    
+          <p> Integrating with BTN Syariah </p>
+          <hr class=" border-black mb-4">
+          <p class="font-thin"> Integration between Bank BTN Syariah (BTNS) and PT Makara Mas as the switcher, using ISO8583 as the message format. One of the hardest task was creating and connecting the machine between two company was done by Mr. Gladhi Guarddin as Senior Consultant of PT Makara Mas. My responsible was to continue, implement needed service to connect them together end-to-end, and finally make a new duplicate of Edupay for BTNS  </p>
+        </div>
+
+  
+      </div>   
       
     </section>
     
