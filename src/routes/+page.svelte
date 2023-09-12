bd193d54bbab6e23e94bb2fb7310958f0d64d6dc
username <user@com.com>
2023-09-12 21:09:49 +0800

    update using sveltekit 4

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import About from "./components/content/about.svelte";
+	import Blog from "./components/content/blog.svelte";
+	import Contact from "./components/content/contact.svelte";
+    import Hero from "./components/content/hero.svelte";
+	import Projects from "./components/content/projects.svelte";
+	import WorkExperience from "./components/content/work-experience.svelte";
+</script>
+
+<Hero /> 
+<About/>
+<Projects/>
+<WorkExperience />
+<Blog />
+<Contact/>
+
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,30 @@
-module.exports = {
-    content: ['./src/**/*.{html,js,svelte,ts}'],
+/** @type {import('tailwindcss').Config}*/
+const config = {
+    content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+  
+    plugins: [require('flowbite/plugin')],
+  
+    darkMode: 'class',
+  
     theme: {
-        container: {
-            center: true,
-            padding: '16px'
-        },
-        extend: {
-            colors: {
-                primary: '#8b5cf6',
-                secondary: '#64748b'
-            }
+      extend: {
+        colors: {
+          // flowbite-svelte
+          primary: {
+            50: '#FFF5F2',
+            100: '#FFF1EE',
+            200: '#FFE4DE',
+            300: '#FFD5CC',
+            400: '#FFBCAD',
+            500: '#FE795D',
+            600: '#EF562F',
+            700: '#EB4F27',
+            800: '#CC4522',
+            900: '#A5371B'
+          }
         }
-    },
-    plugins: []
-};
+      }
+    }
+  };
+  
+  module.exports = config;
